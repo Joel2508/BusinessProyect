6ca13029210ab234c01dc43d16a3f6922236bbfe
ing.joel2508 joel <xtudia.ing.joelarias@hotmail.com>
2017-11-02 09:06:34 -0700

    Create the solution sereval proyect

---
+++ Domain/Model/Business.cs
@@ -0,0 +1,48 @@
+﻿namespace Domain.Model
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Business
+    {
+        [Key]
+        public int BusinessId { get; set; }
+
+        public int TypeBusinessId { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        public string Image { get; set; }
+
+        [Display(Name ="Name")]
+        [Required(ErrorMessage ="The filed {0} is required")]
+        [MaxLength(15,ErrorMessage = "The filed {0} you must enter maximum in character is")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The filed {0} is required")]
+        [MaxLength(20, ErrorMessage = "The filed {0} you must enter maximum in character is")]
+        [Index("Index_PhoneBusiness_RNC",Order =1, IsUnique =true)]
+        public string RNC { get; set; }
+
+        [Required(ErrorMessage = "The filed {0} is required")]
+        [MaxLength(20, ErrorMessage = "The filed {0} you must enter maximum in character is")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "The filed {0} is required")]
+        [MaxLength(20, ErrorMessage = "The filed {0} you must enter maximum in character is")]
+        [Index("Index_PhoneBusiness_RNC", Order =2, IsUnique = true)]
+        [Display(Name = "Phone Business")]
+        public string PhoneBusiness { get; set; }
+
+        [Required(ErrorMessage = "The filed {0} is required")]
+        [MaxLength(20, ErrorMessage = "The filed {0} you must enter maximum in character is")]
+        public string Latitude { get; set; }
+
+        [Required(ErrorMessage = "The filed {0} is required")]
+        [MaxLength(20, ErrorMessage = "The filed {0} you must enter maximum in character is")]
+        public string Lengthe { get; set; }
+
+        public virtual TypeBusiness TypeBusiness { get; set; }
+    }
+}
---
+++ Domain/Model/TypeBusiness.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Domain.Model
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class TypeBusiness
+    {
+        [Key]
+        public int TypeBusinessId { get; set; }
+        [Display(Name ="Type")]
+        [MaxLength(15,ErrorMessage = "The filed {0} you must enter maximum in characteres {1}")]
+        [Required(ErrorMessage ="This filed is required")]
+        public string Type { get; set; }
+
+        public virtual ICollection<Business> Business { get; set; }
+    }
+}
