4f4844d5b9632d3f27f7cd54c7d3007bc16b3522
Joel2508 <Joelarias2508@gmail.com>
2017-11-18 17:42:03 +0100

    Function the map in my project

--- BusinessProyect/BusinessProyect/Pages/OneBusinessMapPage.xaml.cs
+++ BusinessProyect/BusinessProyect/Pages/OneBusinessMapPage.xaml.cs
@@ -1,20 +1,44 @@
-﻿using System;
+﻿using BusinessProyect.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace BusinessProyect.Pages
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class OneBusinessMapPage : ContentPage
-	{
-		public OneBusinessMapPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OneBusinessMapPage : ContentPage
+    {
+        private GeolocatorService geolocatorService;
+
+
+        public OneBusinessMapPage()
+        {
+            InitializeComponent();
+            geolocatorService = new GeolocatorService();
+            MoveMapToCurrentPosition();
+        }
+        #region Methods
+        async void MoveMapToCurrentPosition()
+        {
+            await geolocatorService.GetLocation();
+            if (geolocatorService.Latitude != 0 ||
+                geolocatorService.Longitude != 0)
+            {
+                var position = new Position(
+                    geolocatorService.Latitude,
+                    geolocatorService.Longitude);
+                MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(
+                    position,
+                    Distance.FromKilometers(.5)));
+            }
+        }
+        #endregion
+
+    }
 }
